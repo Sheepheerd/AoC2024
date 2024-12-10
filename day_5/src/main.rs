6484c6b915694f219ae10498e1e461f31530d968
Sheepheerd <sheepheerd@novastar>
2024-12-10 04:26:30 -0500

    Day 5 part a finished

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -5,6 +5,62 @@ fn main() {
     println!("{:?}", rules);
     println!("");
     println!("{:?}", updates);
+
+    let mut num_safe = 0;
+    let mut safe_updates: Vec<Vec<i32>> = Vec::new();
+    for update in updates {
+        if check_valid_update(&rules, update.clone()) {
+            //println!("{:?}", update);
+            safe_updates.push(update);
+            num_safe += 1;
+        }
+    }
+
+    let mut sum_of_middles = 0;
+    for safe_update in safe_updates {
+        let middle = (safe_update.len() / 2);
+        println!("{:?}", safe_update);
+        println!("{}", safe_update[middle]);
+        sum_of_middles += safe_update[middle];
+    }
+    println!("{}", sum_of_middles);
+}
+
+fn check_valid_update(rules: &Vec<(i32, i32)>, update: Vec<i32>) -> bool {
+    let mut working_update: Vec<i32> = Vec::new();
+    let mut is_valid: bool = true;
+    let mut position: usize = 0;
+    for value in update.clone() {
+        if working_update.is_empty() {
+            working_update.push(value);
+            continue;
+        }
+        for pair in rules {
+            let before = pair.0;
+            let after = pair.1;
+            if before != value && position == update.len() - 1 {
+                //println!("{} {}", before, after);
+                if !working_update.contains(&value) {
+                    working_update.push(value);
+                }
+                position += 1;
+            } else if before != value {
+                position += 1;
+                continue;
+            } else if !working_update.contains(&after) {
+                position += 1;
+                if !working_update.contains(&value) {
+                    working_update.push(value);
+                }
+            } else {
+                position += 1;
+                is_valid = false;
+            }
+        }
+        position = 0;
+    }
+    println!("{:?}", working_update);
+    is_valid
 }
 
 fn read_lines(filename: &str) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
